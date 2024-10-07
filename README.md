3c092be6e5d3b45d9c9801d609d3399e75350cc2
valost <valerieostrovska@yahoo.com>
2024-10-07 19:55:31 +0300

    added JS code

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-# Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
-    - [DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+# 2048 GAME
+
+  This is a sliding tile puzzle game written in JavaScript.
+
+  [DEMO LINK](https://valost.github.io/2048-game/)
+
+  # Description
+
+  2048 is a single-player sliding puzzle game. The objective is to slide numbered tiles on a grid to combine them and create a tile with the number 2048. The game continues until there are no free tiles left or the player reaches the winning tile. When two tiles with the same number touch, they merge into a new tile with their values doubled.
+
+  # How to play:
+
+  - Move tiles using arrows (up, down, left, right);
+  - Tiles with the same number will merge into one when they collide;
+  - Keep playing until no more moves are possible or you reach the 2048 tile;
+
+  # Dependencies
+
+  - Node.js v14.21.3
+  - NPM 6.14.18
+
+  # Installation
+
+  - Fork and clone this repository;
+  - Run 'npm install' in you terminal;
+  Run 'npm start' to launch the development server;
+
+  # Troubleshooting
+
+  For common issues, ensure that:
+
+  - you are using the correct Node.js version;
+  - the main dependencies are correctly installed bby running 'npm install';
+  - the development server should start without errors using 'npm start';
+
+  If you encounter any issues or need assistance, feel free to reach out at valostrovska@gmail.com.
+
+
---
+++ src/modules/Game.class.js
@@ -0,0 +1,271 @@
+'use strict';
+
+class Game {
+  static STATUS = {
+    idle: 'idle',
+    playing: 'playing',
+    win: 'win',
+    lose: 'lose',
+  };
+
+  constructor(
+    initialState = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ],
+  ) {
+    this.status = Game.STATUS.idle;
+    this.initialState = initialState;
+    this.state = initialState.map((row) => [...row]);
+    this.score = 0;
+  }
+
+  start() {
+    this.status = Game.STATUS.playing;
+    this.getRandomCells();
+    this.getRandomCells();
+  }
+
+  getRandomCells() {
+    const emptyCells = [];
+
+    for (let r = 0; r < 4; r++) {
+      for (let c = 0; c < 4; c++) {
+        if (this.state[r][c] === 0) {
+          emptyCells.push([r, c]);
+        }
+      }
+    }
+
+    if (emptyCells.length > 0) {
+      const [randomR, randomC] =
+        emptyCells[Math.floor(Math.random() * emptyCells.length)];
+
+      this.state[randomR][randomC] = Math.random() < 0.9 ? 2 : 4;
+    }
+  }
+
+  checkGameStatus() {
+    let canMove = false;
+    let hasEmptyCells = false;
+
+    for (let r = 0; r < 4; r++) {
+      for (let c = 0; c < 4; c++) {
+        if (this.state[r][c] === 2048) {
+          this.status = Game.STATUS.win;
+
+          return;
+        }
+
+        if (this.state[r][c] === 0) {
+          hasEmptyCells = true;
+        }
+
+        if (
+          (r < 3 && this.state[r][c] === this.state[r + 1][c]) ||
+          (c < 3 && this.state[r][c] === this.state[r][c + 1])
+        ) {
+          canMove = true;
+        }
+      }
+    }
+
+    if (!canMove && !hasEmptyCells) {
+      this.status = Game.STATUS.lose;
+    }
+  }
+
+  moveLeft() {
+    if (this.status === Game.STATUS.playing) {
+      let canMove = false;
+
+      for (let r = 0; r < 4; r++) {
+        const values = [];
+
+        for (let c = 0; c < 4; c++) {
+          if (this.state[r][c] !== 0) {
+            values.push(this.state[r][c]);
+          }
+        }
+
+        for (let i = 0; i < values.length; i++) {
+          if (values[i] === values[i + 1]) {
+            values[i] *= 2;
+            values[i + 1] = 0;
+            this.score += values[i];
+            canMove = true;
+          }
+        }
+
+        const updatedRow = values.filter((value) => value !== 0);
+
+        while (updatedRow.length < 4) {
+          updatedRow.push(0);
+        }
+
+        for (let c = 0; c < 4; c++) {
+          if (this.state[r][c] !== updatedRow[c]) {
+            this.state[r][c] = updatedRow[c];
+            canMove = true;
+          }
+        }
+      }
+
+      if (canMove) {
+        this.getRandomCells();
+        this.checkGameStatus();
+      }
+    }
+  }
+
+  moveRight() {
+    if (this.status === Game.STATUS.playing) {
+      let canMove = false;
+
+      for (let r = 0; r < 4; r++) {
+        const values = [];
+
+        for (let c = 3; c >= 0; c--) {
+          if (this.state[r][c] !== 0) {
+            values.push(this.state[r][c]);
+          }
+        }
+
+        for (let i = 0; i < values.length; i++) {
+          if (values[i] === values[i + 1]) {
+            values[i] *= 2;
+            values[i + 1] = 0;
+            this.score += values[i];
+            canMove = true;
+          }
+        }
+
+        const updatedRow = values.filter((value) => value !== 0);
+
+        while (updatedRow.length < 4) {
+          updatedRow.push(0);
+        }
+
+        for (let c = 0; c < 4; c++) {
+          if (this.state[r][c] !== updatedRow[3 - c]) {
+            this.state[r][c] = updatedRow[3 - c];
+            canMove = true;
+          }
+        }
+      }
+
+      if (canMove) {
+        this.getRandomCells();
+        this.checkGameStatus();
+      }
+    }
+  }
+
+  moveUp() {
+    if (this.status === Game.STATUS.playing) {
+      let canMove = false;
+
+      for (let c = 0; c < 4; c++) {
+        const values = [];
+
+        for (let r = 0; r < 4; r++) {
+          if (this.state[r][c] !== 0) {
+            values.push(this.state[r][c]);
+          }
+        }
+
+        for (let i = 0; i < values.length; i++) {
+          if (values[i] === values[i + 1]) {
+            values[i] *= 2;
+            values[i + 1] = 0;
+            this.score += values[i];
+            canMove = true;
+          }
+        }
+
+        const updatedColumn = values.filter((value) => value !== 0);
+
+        while (updatedColumn.length < 4) {
+          updatedColumn.push(0);
+        }
+
+        for (let r = 0; r < 4; r++) {
+          if (this.state[r][c] !== updatedColumn[r]) {
+            this.state[r][c] = updatedColumn[r];
+            canMove = true;
+          }
+        }
+      }
+
+      if (canMove) {
+        this.getRandomCells();
+        this.checkGameStatus();
+      }
+    }
+  }
+
+  moveDown() {
+    if (this.status === Game.STATUS.playing) {
+      let canMove = false;
+
+      for (let c = 0; c < 4; c++) {
+        const values = [];
+
+        for (let r = 3; r >= 0; r--) {
+          if (this.state[r][c] !== 0) {
+            values.push(this.state[r][c]);
+          }
+        }
+
+        for (let i = 0; i < values.length; i++) {
+          if (values[i] === values[i + 1]) {
+            values[i] *= 2;
+            values[i + 1] = 0;
+            this.score += values[i];
+            canMove = true;
+          }
+        }
+
+        const updatedColumn = values.filter((value) => value !== 0);
+
+        while (updatedColumn.length < 4) {
+          updatedColumn.push(0);
+        }
+
+        for (let r = 0; r < 4; r++) {
+          if (this.state[r][c] !== updatedColumn[3 - r]) {
+            this.state[r][c] = updatedColumn[3 - r];
+            canMove = true;
+          }
+        }
+      }
+
+      if (canMove) {
+        this.getRandomCells();
+        this.checkGameStatus();
+      }
+    }
+  }
+
+  restart() {
+    this.status = Game.STATUS.idle;
+    this.state = this.initialState.map((row) => [...row]);
+    this.score = 0;
+  }
+
+  getScore() {
+    return this.score;
+  }
+
+  getState() {
+    return this.state;
+  }
+
+  getStatus() {
+    return this.status;
+  }
+}
+
+module.exports = Game;
